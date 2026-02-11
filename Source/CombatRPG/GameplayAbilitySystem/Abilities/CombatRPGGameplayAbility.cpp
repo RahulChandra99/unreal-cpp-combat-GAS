4a7ec2784df9853d26cade859f58e49bcd21dd0e
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2026-02-11 16:01:24 +0000

    clean

--- Source/CombatRPG/GameplayAbilitySystem/Abilities/CombatRPGGameplayAbility.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CombatRPGGameplayAbility.h"
-
-UCombatRPGGameplayAbility::UCombatRPGGameplayAbility()
-{
-	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("GameplayAbility.Active")));
-}
